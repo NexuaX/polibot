2d041e882609f76bf6d2c8541f3636da470d216c
Mateusz Nosal <mateusz.nosal@vp.pl>
2022-04-10 21:45:32 +0200

    new / app router modify / index php now uses router new / database handler

---
+++ backend/DBHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Classes\Handlers;
+
+use PDOException;
+
+class DBHandler{
+        private \PDO $conn;
+        private string $host;
+        private int $port;
+        private string $dbname;
+        private string $user;
+        private string $passwd;
+
+        //host=sheep port=5432 dbname=test user=lamb password=bar
+        public function __construct()
+        {
+            $this->host = \HOST;
+            $this->port = \PORT;
+            $this->dbname = \DATABASE;
+            $this->user = \USERNAME;
+            $this->passwd = \PASSWORD;
+        }
+        public function connect() : \PDO{
+            if(isset($this->conn))
+                return $this->conn;
+            try{
+                $conn_config = "pgsql:host=$this->host port=$this->port dbname=$this->dbname";
+                $this->conn = new \PDO(
+                    $conn_config,
+                    $this->user,
+                    $this->passwd,
+                    ["sslmode" => "prefer"]
+                );
+
+                $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                return $this->conn;
+            }
+            catch(PDOException $e){
+                die("Connection failed". $e->getMessage());
+            }
+        }
+    }
