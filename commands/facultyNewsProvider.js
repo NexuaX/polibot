6912390a1d262e0ceeba1134a5ccf3ccffa11798
Ivan Morhaliuk <ivan.morhaliuk@gmail.com>
2022-05-10 11:49:12 +0200

    faculty_message feature: refactor using node-fetch

--- commands/facultyNewsProvider.js
+++ commands/facultyNewsProvider.js
@@ -1,19 +1,54 @@
-const nodeParser = require('node-html-parser');
+const fetch = require("node-fetch");
+const {JSDOM} = require("jsdom");
+const {MessageEmbed} = require("discord.js");
+
+let facultyNewsInterval;
+let lastMessage;
 module.exports = {
     name: 'news',
     description: "shows latest news",
-    execute(message) {
-        let request = require("request");
+    execute(message, args) {
 
-        request(
-            { uri: "https://it.pk.edu.pl" },
-            function(error, response, body) {
-                const page = nodeParser.parse(body);
-                const newsCollection = page.querySelectorAll('[mytype="news"]');
-                const latestNews = newsCollection.at(0).querySelector(".readmetxt").innerText;
-                message.channel.send(latestNews);
-            }
-        );
+        switch (args[1]) {
+            case "on":
+                message.channel.send("enabled");
+                facultyNewsInterval = setInterval(() => {
+                    sendLastNewsToTheChat(message)
+                }, 5000);
+                break;
+            case "off":
+                message.channel.send("disabled");
+                clearInterval(facultyNewsInterval);
+                break;
+        }
 
+    }
+}
+
+async function sendLastNewsToTheChat(message) {
+    const result = await fetch("https://it.pk.edu.pl/");
+    const text = await result.text();
+
+    const parser = new JSDOM(text);
+    const parsedDocument = parser.window.document;
+
+    const newsCollection = parsedDocument.querySelector('[mytype="news"]');
+    const latestNews = newsCollection.querySelector(".readmetxt");
+    const linkContainer = newsCollection.querySelector(".text-right");
+    const link = linkContainer.querySelector('a');
+    let isLinkHidden = linkContainer.innerHTML.includes("visibility:hidden");
+    const newsEmbed = new MessageEmbed()
+        .setTitle("Latest News")
+        .setDescription(latestNews.innerHTML);
+    if (!isLinkHidden){
+        linkHref = link.getAttribute('href');
+        linkHref = linkHref.split(" ").join("%20");
+        newsEmbed.addField('Więcej', linkHref);
+        latestNews.innerHTML += '\n' + linkHref;
+    }
+    if (lastMessage == null || lastMessage.innerHTML !== latestNews.innerHTML) {
+        console.log(latestNews.innerHTML);
+        message.channel.send({embeds: [newsEmbed]});
+        lastMessage = latestNews;
     }
 }
