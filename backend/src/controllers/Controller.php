194675e2e5a14946e1fbaa4ed530a1d7e8ae2bda
Mateusz Nosal <mateusz.nosal@vp.pl>
2022-05-21 14:59:55 +0200

    refactor / Response,Validator class added modify / Controller class uses these mentioned Response - handles response codes and data Validator - extracted code from every class

--- backend/src/controllers/Controller.php
+++ backend/src/controllers/Controller.php
@@ -2,13 +2,20 @@
 
 namespace Classes\Controllers;
 
+use Classes\Controllers\Helpers\Response;
+use Classes\Controllers\Helpers\Validator;
+
 class Controller
 {
     private $method;
     protected $data;
+    protected $validator;
+    protected $response;
 
     public function __construct()
     {
+        $this->validator = new Validator();
+        $this->response = new Response();
         $this->method = $_SERVER['REQUEST_METHOD'];
         $this->getData();
     }
