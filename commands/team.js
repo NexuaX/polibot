570e52ce5a47b413cc963b9da3fac1e1bfdb2dd5
Albert Mouhoubi <a.mouhoubi01@gmail.com>
2022-05-22 19:12:20 +0200

    team command refactor

--- commands/team.js
+++ commands/team.js
@@ -1,30 +1,28 @@
-const { MessageEmbed, MessageAttachment} = require("discord.js");
-const { pfp } = require("../assets/urls.json");
+const { MessageEmbed } = require("discord.js");
+const { pfp, team } = require("../assets/urls.json");
 
 module.exports = {
     name: "team",
     description: "wypisuje twórców bota",
     execute(message, args) {
         message.channel.sendTyping();
-        const teamPicture = new MessageAttachment("assets/team.jpg").setDescription("team");
-        const teammates =
-            `
-            Albert Mouhoubi
-            Mateusz Nosal
-            Jacek Oleksy
-            Mikołaj Michalczak
-            Natalia Nahlik
-            Łukasz Miętka
-            Mateusz Mizak
-            Ivan Morhaliuk
-            `;
+        const teammates = [
+            "Albert Mouhoubi",
+            "Mateusz Nosal",
+            "Jacek Oleksy",
+            "Mikołaj Michalczak",
+            "Natalia Nahlik",
+            "Łukasz Miętka",
+            "Mateusz Mizak",
+            "Ivan Morhaliuk"
+        ];
         const teamEmbed = new MessageEmbed()
             .setTitle("PoliBot Team!")
             .setDescription("To oni nadali mi życie!")
             .setThumbnail(pfp)
-            .addField('team', teammates)
-            .setImage('attachment://team.jpg');
+            .addField('team', teammates.join('\n'))
+            .setImage(team);
 
-        message.channel.send({embeds: [teamEmbed], files: [teamPicture]});
+        message.channel.send({embeds: [teamEmbed]});
     }
 }
